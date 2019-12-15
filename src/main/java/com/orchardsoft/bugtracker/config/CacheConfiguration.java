f58b6138c5e2c71349815d204e179e24ea102cc9
David Huertas Ruiz <dhuertas.eng@gmail.com>
2019-12-15 01:36:59 +0100

    Guide steps from 0 to 5

--- src/main/java/com/orchardsoft/bugtracker/config/CacheConfiguration.java
+++ src/main/java/com/orchardsoft/bugtracker/config/CacheConfiguration.java
@@ -45,6 +45,11 @@ public class CacheConfiguration {
             createCache(cm, com.orchardsoft.bugtracker.domain.User.class.getName());
             createCache(cm, com.orchardsoft.bugtracker.domain.Authority.class.getName());
             createCache(cm, com.orchardsoft.bugtracker.domain.User.class.getName() + ".authorities");
+            createCache(cm, com.orchardsoft.bugtracker.domain.Project.class.getName());
+            createCache(cm, com.orchardsoft.bugtracker.domain.Label.class.getName());
+            createCache(cm, com.orchardsoft.bugtracker.domain.Label.class.getName() + ".tickets");
+            createCache(cm, com.orchardsoft.bugtracker.domain.Ticket.class.getName());
+            createCache(cm, com.orchardsoft.bugtracker.domain.Ticket.class.getName() + ".labels");
             // jhipster-needle-ehcache-add-entry
         };
     }
---
+++ src/main/java/com/orchardsoft/bugtracker/repository/LabelRepository.java
@@ -0,0 +1,14 @@
+package com.orchardsoft.bugtracker.repository;
+import com.orchardsoft.bugtracker.domain.Label;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * Spring Data  repository for the Label entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface LabelRepository extends JpaRepository<Label, Long> {
+
+}
